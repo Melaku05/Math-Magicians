aae3f9f3e2939548bce97bfe23e67e02af07a55b
Melaku <melakushiferaw05@gmail.com>
2022-05-04 15:26:25 +0300

    added Navbar and footer pages

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+const Footer = () => (
+  <div className="footer">
+    <p>
+      {' '}
+      Designed and coded by:
+      {' '}
+      <a href="https://github.com/Mesi21">@Mesi21</a>
+    </p>
+  </div>
+);
+
+export default Footer;
---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Home = () => (
+  <div id="home">
+    <h1>Welcome to our page!</h1>
+    <section>
+      <p>
+        Curabitur pretium orci sit amet mauris pulvinar malesuada. Sed tellus sem,
+        mollis rhoncus elementum eget, sagittis ut ante.
+        In aliquet nisi velit, eget cursus dolor aliquet sed.
+        Cras at mollis libero. Quisque fermentum diam vel ante sodales dictum.
+        Aliquam fermentum dignissim consequat.
+        Morbi aliquam leo a mi tristique, vel culis neque pellentesque. Nullam sed malesuada sem.
+        Sed tempus, nulla sed gravida pharetra, mi odio eleifend massa, quis congue nunc
+        sapien eu nulla.
+        Morbi porttitor odio at mollis dignissim.
+      </p>
+      <p>
+        Vestibulum laoreet elit turpis, a suscipit leo ullamcorper eget. Donec tincidunt
+        diam ut sapien eleifend, scelerisque convallis odio ultrices. Mauris condimentum purus
+        non risus suscipit suscipit. Vestibulum ante ipsum primis in faucibus orci luctus et
+        ultrices posuere cubilia curae; Praesent quis nibh eget risus imperdiet loret. Sed maximus
+        posuere quam ut scelerisque. Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Cras pellentesque ullamcorper nisl, et consequat ligula imperdiet id.
+        Praesent egestas augue lectus, ac faucibus metus tristique nec. Donec sed ex ultrices,
+        laciniasapien eu, laoreet est.
+      </p>
+    </section>
+  </div>
+);
+
+export default Home;
